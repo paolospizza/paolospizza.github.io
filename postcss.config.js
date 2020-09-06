fe9d9dd34fa229b415ba5cf38d3bdc52b6289145
Paul D'Ambra <paul.damba@coop.co.uk>
2020-09-06 11:31:24 +0100

    deploy to prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,8 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-        './docs/index.html',
-    ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
 
 module.exports = {
     plugins: [
         require('tailwindcss')('./tailwind.config.js'),
         require('cssnano')(),
-        require('autoprefixer'),
-        ...process.env.NODE_ENV === 'production'
-            ? [purgecss]
-            : []
+        require('autoprefixer')
     ]
 };
