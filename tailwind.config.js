4f925913355619a159a1df2311b3268dcd8a2cbc
Paul D'Ambra <paul.damba@coop.co.uk>
2020-11-23 21:40:19 +0000

    switch all text to gentium

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./_site/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      serif: ['Gentium Basic', ...defaultTheme.fontFamily.serif]
+    },
     extend: {},
   },
   variants: {
