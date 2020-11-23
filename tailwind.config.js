5ad00ca4bc392c18ea9af4f0aa582412f4b31fd5
pauldambra <pauldambra@users.noreply.github.com>
2020-11-23 22:10:52 +0000

    deploy: f541e02b6b93d3ff9a24bb5ff09ec0049c228be5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./_site/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      serif: ['Gentium Basic', ...defaultTheme.fontFamily.serif],
+      cursive: ['Cookie']
+    },
     extend: {},
   },
   variants: {
